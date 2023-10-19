f84146a89d562dfc5c643ccec87ce9a4ebf3b955
Satoshi Yano <remember3939@gmail.com>
2023-10-19 14:32:31 +0900

    ソースコード追加

--- main.cpp
+++ main.cpp
@@ -34,5 +34,7 @@ void DrawMain()
     DrawText("Hello, world!!", -12*14/2, -44, kColorRed);
     Sleep(0.5f);
     DrawText("Hello, world!!", -12*14/2, -66, kColorGray);
+    
+    FillCircle(0, 0, 20, kcolorRed);
 }
 

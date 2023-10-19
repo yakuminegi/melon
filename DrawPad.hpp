916b45bf53540ea079a0834641be4be0824af494
Junya Ueno <hb21a014@oecu.jp>
2023-10-19 14:33:24 +0900

    Revert "First Commint of melon"
    This reverts commit c30cf274791ea85ef74cb241c12acb4c51cb2f27.

--- DrawPad.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef DrawPad_h
-#define DrawPad_h
-
-#include "Drawing.hpp"
-#include "DrawingVector.hpp"
-
-#endif /* DrawPad_h */
--- MyViewController.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-@interface MyViewController : NSViewController
-@end
-
